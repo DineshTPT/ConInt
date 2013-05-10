4cb99e0739a17b938072f444f2d84c897e4405ae
Dinesh Patil <dinesh.patil@tpt.com>
2013-05-10 18:16:44 +0530

    asd

--- test.sh
+++ test.sh
@@ -3,7 +3,6 @@
 
 MARKETTING_VERSION="1.0"
 TARGET_SDK="iphoneos6.1"
-BUILD_HISTORY_DIR="~/Desktop"
 API_TOKEN="68730198beebeebed04363cbf48300cd_OTMzNjI4MjAxMy0wMy0xNSAwOToxNzo0OS4zMjM0OTU"
 TEAM_TOKEN="4b7d84ffb8df9dd918918feae21c98e5_MjIwNDYyMjAxMy0wNS0wOSAxMTo1MDo0NC45NzAzMjY"
 
@@ -11,8 +10,8 @@ for config in $CONFIGURATIONS; do
 PROVISONNING_PROFILE=$(eval echo \$`echo ProvisionFile$config`)
 DEVELOPPER_NAME=$(eval echo \$`echo Codesign$config`)
 
-PROJECT_BUILDDIR="build/${$config}-iphoneos"
-echo PROJECT_BUILDDIR
+PROJECT_BUILDDIR="build/${config}-iphoneos"
+echo $PROJECT_BUILDDIR
 
 XCODE_PROJECT_FILE_NAME=$(ls -d *.xcodeproj)
 PROJECT_NAME=$(eval echo $XCODE_PROJECT_FILE_NAME |cut -d'.' -f1)
@@ -36,7 +35,7 @@ exit 1
 fi
 
 app_path=$(ls -d build/$config-iphoneos/*.app)
-/usr/bin/xcrun -sdk iphoneos PackageApplication -v "${app_path}" -o "$(pwd)/${PROJECT_NAME}.ipa" --sign "${DEVELOPPER_NAME}" --embed "${PROVISONNING_PROFILE}"
+/usr/bin/xcrun -sdk iphoneos PackageApplication -v "${app_path}" -o "$(pwd)/${PROJECT_NAME}.ipa" --sign "${DEVELOPPER_NAME}" --embed "$(pwd)/${PROVISONNING_PROFILE}"
 
 #echo -n "Zipping .dSYM for ${PRODUCT_NAME}..." >> $LOG
 echo "Zipping .dSYM for ${PROJECT_NAME}"
