cc046dbf562b6ecdba5148373c34e74535cc6242
Dinesh Patil <dinesh.patil@tpt.com>
2013-05-10 17:41:47 +0530

    modified script

--- test.sh
+++ test.sh
@@ -1,25 +1,26 @@
 
 . "build.config"
 
-for config in $CONFIGURATIONS; do
-provfile=$(eval echo \$`echo ProvisionFile$config`)
-codesign=$(eval echo \$`echo Codesign$config`)
-
-echo $provfile
-echo $codesign
-echo $config
-
-cert="$provfile"
-fileprefix="$config";
-
-PROJECT_NAME="ConInt"
+MARKETTING_VERSION="1.0"
 TARGET_SDK="iphoneos6.1"
 PROJECT_BUILDDIR="build/Debug-iphoneos"
-BUILD_HISTORY_DIR="/Users/Dinesh/Desktop"
-DEVELOPPER_NAME=$codesign
-PROVISONNING_PROFILE=$provfile
+BUILD_HISTORY_DIR="~/Desktop"
+API_TOKEN="68730198beebeebed04363cbf48300cd_OTMzNjI4MjAxMy0wMy0xNSAwOToxNzo0OS4zMjM0OTU"
+TEAM_TOKEN="4b7d84ffb8df9dd918918feae21c98e5_MjIwNDYyMjAxMy0wNS0wOSAxMTo1MDo0NC45NzAzMjYxx"
 
-agvtool new-marketing-version 1.1.2
+for config in $CONFIGURATIONS; do
+PROVISONNING_PROFILE=$(eval echo \$`echo ProvisionFile$config`)
+DEVELOPPER_NAME=$(eval echo \$`echo Codesign$config`)
+
+XCODE_PROJECT_FILE_NAME=$(ls -d *.xcodeproj)
+PROJECT_NAME=$(eval echo $XCODE_PROJECT_FILE_NAME |cut -d'.' -f1)
+
+echo $PROJECT_NAME
+echo $PROVISONNING_PROFILE
+echo $DEVELOPPER_NAME
+
+# Auto increment build number
+agvtool new-marketing-version $MARKETTING_VERSION
 agvtool bump -all
 
 # compile project
@@ -35,19 +36,11 @@ fi
 app_path=$(ls -d build/$config-iphoneos/*.app)
 /usr/bin/xcrun -sdk iphoneos PackageApplication -v "${app_path}" -o "$(pwd)/${PROJECT_NAME}.ipa" --sign "${DEVELOPPER_NAME}" --embed "${PROVISONNING_PROFILE}"
 
-
-API_TOKEN="68730198beebeebed04363cbf48300cd_OTMzNjI4MjAxMy0wMy0xNSAwOToxNzo0OS4zMjM0OTU"
-TEAM_TOKEN="4b7d84ffb8df9dd918918feae21c98e5_MjIwNDYyMjAxMy0wNS0wOSAxMTo1MDo0NC45NzAzMjY"
-
-DSYM="${HOME}/Library/Developer/Xcode/Archives/${DATE}/${ARCHIVE}/dSYMs/${PRODUCT_NAME}.app.dSYM"
-APP="${HOME}/Library/Developer/Xcode/Archives/${DATE}/${ARCHIVE}/Products/Applications/${PRODUCT_NAME}.app"
-
-
 #echo -n "Zipping .dSYM for ${PRODUCT_NAME}..." >> $LOG
 echo "Zipping .dSYM for ${PROJECT_NAME}"
 
-dsym_path=$(ls -d build/$config-iphoneos/*.dSYM)
-zip -r "${dsym_path}.zip" "${dsym_path}"
+dSYM_PATH=$(ls -d build/$config-iphoneos/*.dSYM)
+zip -r "${dSYM_PATH}.zip" "${dSYM_PATH}"
 
 #echo "done." >> $LOG
 echo "Created .dSYM for ${PRODUCT_NAME}"
@@ -57,17 +50,17 @@ echo "Uploading to TestFlight"
 
 curl "http://testflightapp.com/api/builds.json" \
 -F file=@"${PROJECT_NAME}.ipa" \
--F dsym=@"${dsym_path}.zip" \
+-F dsym=@"${dSYM_PATH}.zip" \
 -F api_token="${API_TOKEN}" \
 -F team_token="${TEAM_TOKEN}" \
 -F notes="Build uploaded automatically using build script."
 
-#check if the upload was successful, else retry upload next time. clean up on successful upload.
+if [ $? != 0 ]
+then
+echo "upload failed"
+exit 1
+fi
 
-#echo "done." >> $LOG
-echo "Uploaded to TestFlight"
-
-
-#few changes + few more
+echo "Build uploaded to TestFlight successfully!"
 
 done
